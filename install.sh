277fdc5c737cec1bc857559a5269b067cdb3d5a0
Yann Vallery-Radot <yannvr@gmail.com>
2016-11-06 00:18:25 +0000

    Jagerweg install! Get setup for coding from ground zero

--- install.sh
+++ install.sh
@@ -1,44 +1,22 @@
-#!/bin/zsh
-#TODO: Input git author settings, don't hardcode it!
+#!/usr/bin/env zsh
+
+echo -e '\n\n\n\n\n\n\n\n\n\n'
+cat banner.txt
+echo -e '\n\n\n\n\n\n\n\n\n\n'
+
+# unalias date just in case
+unalias date 2&>1 > /dev/null
 
 date=`date +%d_%m_%Y`
 bkpDir="~/bkp/dotfiles-${date}"
 
+
 mkdir -p ${bkpDir}
 echo "Backing up overriden dotfiles in ${bkpDir}"
 
-cd ~/dotfiles
-
-echo "Do you wish to install neobundle?"
-read neobundle
-
-if [ $neobundle = 'y' ]; then
-    curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > /tmp/install.sh;
-    sh /tmp/install.sh
-fi
-
-echo "Install oh-my-zsh?"
-read ohmyzsh
-
-if [ $ohmyzsh = 'y' ]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
-
-
-echo "Install brew dependencies?"
-read brew
-
-if [ $brew= 'y' ]; then
-    brew update
-    brew install nvm
-    brew install neovim
-    brew install fzf
-    brew install ag
-fi
-
 for i in `find .* -depth 0 -type f`; do
-    mv ~/${i} ${bkpDir}
-    ln -s ~/dotfiles/${i} ~/${i}
+    mv ~/${i} ${bkpDir} 2&>1 > /dev/null
+    ln -s ~/dotfiles/${i} ~/${i} 2&>1 > /dev/null
 done
 
 # Takes care of this big gotcha using neovim..
@@ -46,7 +24,82 @@ done
 `ln -s ~/.vimrc ~/.config/nvim/init.vim`
 if [ $? -ne 0  ]; then echo 'LINKING NVIM config to an existing config failed. Ignore '; fi
 
-echo "dotfiles are linked!"
+echo "\ndotfiles are linked!"
 
-echo "Installing vim/neovim bundles.."
-vim -c :NeoBundleInstall
+echo "Do you want to setup your environment?"
+read setup
+
+if [ $setup != 'y' ]; then
+    exit 0
+fi
+
+cd ~/dotfiles
+
+echo "Do you want to author git commit with mail and name? (y/n)"
+read gituser
+
+if [ $gituser = 'y' ]; then
+
+    echo "What is your name? (y/n)"
+    read gitname
+
+    echo "What is your email? (y/n)"
+    read gitemail
+
+    cat >> ~/.gitconfig << EOF
+[user]
+    name = ${gitname}
+    email = ${gitemail}
+EOF
+
+fi
+
+echo "Do you wish to install neobundle? (y/n)"
+read neobundle
+
+if [ $neobundle = 'y' ]; then
+    curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > /tmp/install.sh;
+    sh /tmp/install.sh
+fi
+
+echo "Install oh-my-zsh? (y/n)"
+read ohmyzsh
+
+if [ $ohmyzsh = 'y' ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+
+echo "Install brew dependencies? (y/n)"
+read brew
+
+if [ $brew = 'y' ]; then
+    brew update
+    brew install nvm
+    brew install neovim
+    brew install fzf
+    brew install ag
+fi
+
+echo "You can install Vim/Neovim plugins now or next time you unleash the beast"
+echo "Install vim/neovim plugins now? (y/n)"
+
+read vimplugins
+
+if [ $vimplugins = 'y' ]; then
+    echo "Installing vim/neovim bundles.."
+    vim -c :NeoBundleInstall
+fi
+
+echo "Install Solarized8 (true colors)?"
+
+read solarized8
+
+if [ $solarized8 = 'y' ]; then
+    echo "Installing solarized8"
+    git clone https://github.com/lifepillar/vim-solarized8.git/ /tmp/vim-solarized8
+    mkdir ~/.vim/colors 2&>1 > /dev/null
+    mv /tmp/vim-solarized8/colors/*.vim ~/.vim/colors
+fi
+
+echo -e "\n\nIt's Jagertime!"
